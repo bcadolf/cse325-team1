43d6cc66377282deb6d11f3ebac35dce95d5f6e9
bcadolf <adolfbrandon96@gmail.com>
2026-02-10 21:27:59 -0500

    progress on add event css

--- Data/Models/EventModel.cs
+++ Data/Models/EventModel.cs
@@ -3,7 +3,7 @@ namespace cse325_team1.Data.Models;
 public class EventModel
 {
     public string title { get; set; }
-    public DateTime dateTime { get; set; }
+    public DateTime dateTime { get; set; } = DateTime.Now;
     public string description { get; set; }
     public string firstName { get; set; }
     public string lastName { get; set; }
